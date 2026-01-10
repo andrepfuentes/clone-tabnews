7c1da9448cf0c4f5587325ba015d94eb5104f419
André Fuentes <125984732+andrepfuentes@users.noreply.github.com>
2026-01-10 12:31:03 +0000

    index modification 2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 export default function Home() {
-  return <h1>Oi Raquel, te amo.</h1>
-  <p>What we'll going to do today?</p>;
+  return <div>
+    <h1>Oi Raquel, te amo.</h1>
+    <p>What we'll going to do today?</p>
+  </div>;
 }
