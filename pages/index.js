76bca2a6fc2724ed8956dc60fdb57d304a5f1a53
André Fuentes <125984732+andrepfuentes@users.noreply.github.com>
2026-01-11 10:18:33 +0000

    add scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 export default function Home() {
-  return <div>
-    <h1>Oi Raquel, te amo.</h1>
-    <p>What are we going to do today?</p>
-  </div>;
+  return (
+    <div>
+      <h1>Oi Raquel, te amo.</h1>
+      <p>What are we going to do today?</p>
+    </div>
+  );
+}
+
+function teste1() {
+  console.log("wrong indent");
 }
